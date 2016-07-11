84def836154d1b78f98999e2bcffedeaf985dfbf
Miclle <miclle.zheng@gmail.com>
2016-07-11 14:41:38 +0800

    Add Etag.

---
+++ etag_test.go
@@ -0,0 +1,14 @@
+package imginfo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetEtag(t *testing.T) {
+	assert := assert.New(t)
+	etag, err := GetEtag("./testdata/cLena.bmp")
+	assert.Nil(err)
+	assert.Equal(etag, "FkNeoBle1hbDNe71uzruZSc3Qv-W")
+}
